1e0cfde5e8a711c6b55dc70c518fa51965088ff7
divrawat <divrawat2001@gmail>
2024-08-29 00:33:52 +0530

    drg0er5g

--- pages/_document.js
+++ pages/_document.js
@@ -7,14 +7,18 @@ export default function Document() {
       <Head>
         <meta name="monetag" content="ce71a51c7dced9d34e9002cef042aaf5" />
 
-        <Script
+        {/* <Script
           id="monetag-script"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(s,u,z,p){s.src = u, s.setAttribute('data-zone', z), p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7725769,document.body||document.documentElement)`,
           }}
-        />
+        /> */}
 
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script async crossOrigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1721485376950080" />
+        <script async data-ad-client="ca-pub-1721485376950080" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" ></script>
 
       </Head>
       <body>
